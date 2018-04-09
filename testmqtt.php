40c24565491f8cae7e33845629c9b8fb4a41e53a
ps2005 <n1ebula2@ghostmail.com>
2018-04-09 23:16:43 +0700

    Update testmqtt.php

--- testmqtt.php
+++ testmqtt.php
@@ -1,14 +1,18 @@
 <?php
 require('vendor/autoload.php');
+require("phpMQTT.php");
 
-$url = parse_url(getenv('CLOUDMQTT_URL'));
-$topic = substr($url['path'], 1);
-$client_id = "phpMQTT-publisher";
+$server = "m13.cloudmqtt.com";     // change if necessary
+$port = 12427;                     // change if necessary
+$username = "view";                   // set your username
+$password = "1234";                   // set your password
+$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $message = "Hello CloudMQTT!";
 
-$mqtt = new Bluerhinos\phpMQTT($url['m13.cloudmqtt.com'], $url['12427'], $client_id);
-if ($mqtt->connect(true, NULL, $url['view'], $url['1234'])) {
+$mqtt = new phpMQTT($server, $port, $client_id);
+//$mqtt = new Bluerhinos\phpMQTT($url['m13.cloudmqtt.com'], $url['12427'], $client_id);
+if ($mqtt->connect(true, NULL, $username, $password)) {
     $mqtt->publish($topic, $message, 0);
     echo "Published message: " . $message;
     $mqtt->close();
