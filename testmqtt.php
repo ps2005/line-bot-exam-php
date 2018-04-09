902409cd5897a299efb15543a155cceba425d800
ps2005 <n1ebula2@ghostmail.com>
2018-04-09 23:22:11 +0700

    Update testmqtt.php

--- testmqtt.php
+++ testmqtt.php
@@ -6,7 +6,7 @@ $server = "m13.cloudmqtt.com";     // change if necessary
 $port = 12427;                     // change if necessary
 $username = "view";                   // set your username
 $password = "1234";                   // set your password
-$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
+$client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $topic="/IOT";
 $message = "Hello CloudMQTT!";
