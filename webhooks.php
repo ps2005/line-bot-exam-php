d5771294079cee2a43ae6b9517b55793b3b354d6
ps2005 <n1ebula2@ghostmail.com>
2018-04-17 11:26:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -54,8 +54,37 @@ if(isset($res['events']) && !is_null($res['events'])){
  if($item['type'] == 'message'){
  switch($item['message']['type']){
  case 'text':
-   $packet = posttext($item['replyToken'],$item['message']['text']);
-   postMessage($access_token,$packet,$urlReply);
+			// Get replyToken
+			$replyToken = $item['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => "ddd"
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";		 
+		 
+ //  $packet = posttext($item['replyToken'],$item['message']['text']);
+ //  postMessage($access_token,$packet,$urlReply);
 break;
 case 'image':
 
